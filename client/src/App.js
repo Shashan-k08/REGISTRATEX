650c7969d627f31d0020008a42d5415ecd9b3fd9
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-04-27 22:07:14 +0530

    .

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+          Learn React
+  
+      </header>
+    </div>
+  );
+}
+
+export default App;
