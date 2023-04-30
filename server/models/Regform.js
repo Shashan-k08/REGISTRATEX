1a69be1d57dfccd3b6861ee1547dce2cea545e83
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-04-30 18:26:12 +0530

    reg-schema

---
+++ server/models/Regform.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const RegisterSchema = new Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: String,
+        required: true,
+
+    },
+    email: {
+        type: String,
+        required: true
+    },
+
+    cmail: {
+        type: String,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true
+    },
+    
+    gender: {
+        type: String,
+        required: true
+    },
+    studentno: {
+        type: String,
+        required: true
+    },
+    unirollno: {
+        type: String,
+        required: true
+    },
+    cranch: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+module.exports = mongoose.model(RegisterSchema);
